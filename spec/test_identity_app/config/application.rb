993917b61d2e364babf92d61fa1e97bf4c764fe1
Michael Gratton <michaelg@getup.org.au>
2023-07-13 14:33:17 +1000

    Supporting getting logging calls printed to stdout in all environments

--- spec/test_identity_app/config/application.rb
+++ spec/test_identity_app/config/application.rb
@@ -13,6 +13,12 @@ module TestIdentityApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    if ENV["RAILS_LOG_TO_STDOUT"].present?
+      logger = ActiveSupport::Logger.new(STDOUT)
+      logger.formatter = config.log_formatter
+      config.logger = ActiveSupport::TaggedLogging.new(logger)
+      config.active_record.logger = nil
+    end
   end
 end
-
