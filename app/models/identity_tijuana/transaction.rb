a9666cbdb82ed08e4928c81b418fa99e8f3b06ed
Ivan Buljan <ivanb@getup.org.au>
2024-10-24 10:54:23 +1100

    Track modified donations based on transaction updates

--- app/models/identity_tijuana/transaction.rb
+++ app/models/identity_tijuana/transaction.rb
@@ -1,6 +1,12 @@
 module IdentityTijuana
   class Transaction < ReadWrite
     self.table_name = 'transactions'
-    belongs_to :donation, optional: true
+    belongs_to :donation, optional: false
+
+    scope :updated_transactions_all, ->(last_updated_at, last_id, exclude_from) {
+      where('updated_at > ? OR (updated_at = ? AND id > ?)',
+            last_updated_at, last_updated_at, last_id)
+        .where(updated_at: ...exclude_from)
+    }
   end
 end
